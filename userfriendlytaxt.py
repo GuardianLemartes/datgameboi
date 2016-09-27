9c23ceef424d5dfccd75b70f55f7910f9739ef44
Luke <deadspace8986@gmail.com>
2016-09-27 13:18:03 -0500

    Create userfriendlytaxt.py

---
+++ userfriendlytaxt.py
@@ -0,0 +1,9 @@
+import time
+print ("welcome to pixel world, first you need to pick a class")
+time.sleep(3)
+playerclass = input ("what class would you like? you can make any class you want ")
+print ("your " + playerclass + " is going to be great")
+time.sleep(3)
+print ("now that you are a " + playerclass + " youre going to need a weapon, so what would that be?\n")
+playerweapon = input ("pick your weapon of choice")
+print ("the " + playerweapon + " seems pretty cool, but ive seen better")
